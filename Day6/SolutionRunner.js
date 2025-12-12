1ac6ff23380e7b6ab7ca2da2be5028db8a3ae48f
Ruby Chang <rchang825@hotmail.com>
2025-12-11 19:04:53 -0800

    Day 6 Part 1

---
+++ Day6/SolutionRunner.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+const solution = require('./solution.js');
+
+function parseInput(filePath) {
+    const input = fs.readFileSync(filePath, 'utf-8');
+    const homework = input.split('\n');
+    return homework;
+}
+
+function main() {
+    const inputFilePath = path.join(__dirname, '/input.txt');
+    const homework = parseInput(inputFilePath);
+    let res = solution(homework);
+    console.log('Grand Total:', res);
+}
+
+main();
---
+++ Day6/solution.js
@@ -0,0 +1,59 @@
+function solution(homework) {
+  // number of operands in each problem = total lines - 1
+  // number of problems = number of operation symbols on last line of input
+  const n = homework.length - 1;
+  // console.log(homework);
+  homework = homework.map(line => line.trim().split(/\s+/));
+  let numProblems = homework[n].length;
+  // console.log(homework);
+  // console.log('num Problems', numProblems);
+
+  // does every problem have same number of operands? A: it seems so
+  // let probs = new Set();
+  // homework.forEach((line) => {
+  //   probs.add(line.length);
+  // });
+  // console.log(probs);
+
+  // total
+  let total = 0;
+  // helper solve(column)
+  function solve(column) {
+    // find operation
+    const op = homework[n][column];
+    // define answer
+    let ans = Number(homework[0][column]);
+    // console.log(ans);
+    // +
+    if (op === '+') {
+      // iterate for as many operands there are (total lines - 1)
+      for (let i = 1; i < n; i++) {
+        // console.log(homework[i][column]);
+        // add to answer
+        ans += Number(homework[i][column]);
+      }
+      // console.log('sums to');
+    } else {
+      // *
+      for (let i = 1; i < n; i++) {
+        // console.log(homework[i][column]);
+        // multiply answer by
+        ans *= Number(homework[i][column]);
+      }
+      // console.log('multiplies to');
+    }
+    // return answer
+    // console.log(ans);
+    return ans;
+  }
+  // iterate for as many problems there are
+  for (let i = 0; i < numProblems; i++) {
+    // add answer to total
+    total += solve(i);
+  }
+
+  // return total
+  return total;
+}
+
+module.exports = solution;
